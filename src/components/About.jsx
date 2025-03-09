98e7bf9bdb647aa455f55f66d824317e09af8101
Mahanthb <mahanthkumarb@gmail.com>
2025-03-09 12:14:35 +0530

    commit

---
+++ src/components/About.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import { Container, Col, Row } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import Fade from 'react-reveal';
+import Header from './Header';
+import endpoints from '../constants/endpoints';
+import FallbackSpinner from './FallbackSpinner';
+
+const styles = {
+  introTextContainer: {
+    margin: 10,
+    flexDirection: 'column',
+    whiteSpace: 'pre-wrap',
+    textAlign: 'left',
+    fontSize: '1.2em',
+    fontWeight: 500,
+  },
+  introImageContainer: {
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    display: 'flex',
+  },
+  introImage: {
+    maxWidth: '300px',
+    maxHeight: '300px',
+    width: '100%',
+    height: 'auto',
+    borderRadius: '50%',
+    objectFit: 'cover',
+    boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
+  },
+};
+
+function About(props) {
+  const { header } = props;
+  const [data, setData] = useState(null);
+
+  const parseIntro = (text) => (
+    <ReactMarkdown>
+      {text}
+    </ReactMarkdown>
+  );
+
+  useEffect(() => {
+    fetch(endpoints.about, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <Header title={header} />
+      <div className="section-content-container">
+        <Container>
+          {data ? (
+            <Fade>
+              <Row>
+                <Col style={styles.introTextContainer}>
+                  {parseIntro(data.about)}
+                </Col>
+                <Col style={styles.introImageContainer}>
+                  <img
+                    src={data?.imageSource}
+                    alt="profile"
+                    style={styles.introImage}
+                  />
+                </Col>
+              </Row>
+            </Fade>
+          ) : (
+            <FallbackSpinner />
+          )}
+        </Container>
+      </div>
+    </>
+  );
+}
+
+About.propTypes = {
+  header: PropTypes.string.isRequired,
+};
+
+export default About;
